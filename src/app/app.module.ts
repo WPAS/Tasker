5d7f08e74afd51ac2faf05d9569670cba680fed9
Wojciech Pastuszka <wojciech.pastuszka@gazeta.pl>
2017-10-16 14:10:38 +0200

    add components structure and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { NewTaskComponent } from './new-task/new-task.component';
+import { TaskComponent } from './task/task.component';
+
+const routes = [
+  {path: '', component: TasksComponent},
+  {path: 'new-task', component: NewTaskComponent},
+  {path: '**', redirectTo: '/'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TasksComponent,
+    NewTaskComponent,
+    TaskComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
